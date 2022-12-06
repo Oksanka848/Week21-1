8015fae53080d36bd72d951d78ffdfc37511d8d3
Oksanka848 <okmitka@mail.ru>
2022-12-06 11:33:32 +0000

    ch

--- form.js
+++ form.js
@@ -5,9 +5,10 @@ let midname = document.getElementById ('midname');
 let city = document.getElementById ('city');
 let mail = document.getElementById ('mail');
 let bfday = document.getElementById ('bfday');
+let login = document.getElementById ('login');
 button.addEventListener("click", function() {
     checkAll();
-    ValidateEmail(email);
+    ValidateEmail(mail);
     checkDate(bfday);
   })
 
@@ -35,10 +36,13 @@ if (mail.value == '') {
 if (bfday.value  == '') {
 document.getElementById ('error').innerHTML+= "Ваша дата рождения?<br>";
 } 
+if (login.value  == '') {
+    document.getElementById ('error').innerHTML+= "Ваш логин?<br>";
+    } 
 }
 }
 
-function ValidateEmail(email) {
+function ValidateEmail(mail) {
     let mailFormat = "[a-z0-9._%+-]+@[ a-z0-9.-]+.[a-z]{2,}$";
     if (mail.value.match(mailFormat)) {
         return true;
@@ -49,7 +53,7 @@ function ValidateEmail(email) {
     }
 }
 
-function checkDate(birthday) {
+function checkDate(bfday) {
 var date_regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
 if (bfday.value.match(date_regex)) {
     /*alert("valid date");*/
